eabcdb553dbe62b5f0a6a9bbd43a119ae2c14122
agpalberti <agonzalezp@iesrafaelalberti.es>
2021-10-29 19:44:11 +0200

    segundo

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,26 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
 
-    // Try adding program arguments at Run/Debug configuration
-    println("Program arguments: ${args.joinToString()}")
+class Persona(val DNI: String){
+
+    var nombre:String = ""
+    val edad:Int = 0
+    val sexo:Char = 'M'
+    val peso:Float = 0.0F
+    val altura: Float = 0.0F
+
+    init {
+    }
+
+    constructor(DNI:String, nombre: String,edad: Int, sexo: Char):this(DNI){
+        this.nombre = nombre
+
+    }
+
+    constructor(DNI: String, nombre:String, edad:Int, sexo:Char, peso:Float, altura: Float):this(DNI)
+}
+
+
+
+fun main(){
+
+    val yo = Persona("550428F")
 }
