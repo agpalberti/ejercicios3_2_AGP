243d5feb8893ca1e4571efea53717b67883290be
agpalberti <agonzalezp@iesrafaelalberti.es>
2021-10-29 20:49:53 +0200

    privates

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,26 +1,42 @@
 
 class Persona(val DNI: String){
 
-    var nombre:String = ""
-    val edad:Int = 0
-    val sexo:Char = 'M'
-    val peso:Float = 0.0F
-    val altura: Float = 0.0F
-
-    init {
-    }
+    private var nombre:String = ""
+    private var edad:Int = 0
+    private var sexo:Char = 'H'
+        set(value){
+            if(value == 'H' || value == 'M') field = value
+            else throw IllegalArgumentException("El sexo solo puede ser H o M.")
+        }
+    private var peso:Float = 0.0F
+    private var altura: Float = 0.0F
 
     constructor(DNI:String, nombre: String,edad: Int, sexo: Char):this(DNI){
         this.nombre = nombre
+        this.edad = edad
+        this.sexo = sexo
+    }
+
+    constructor(DNI: String, nombre:String, edad:Int, sexo:Char, peso:Float, altura: Float):this(DNI){
+        this.nombre = nombre
+        this.edad = edad
+        this.sexo = sexo
+        this.peso = peso
+        this.altura = altura
+    }
+
+    fun calcularIMC(){
 
     }
 
-    constructor(DNI: String, nombre:String, edad:Int, sexo:Char, peso:Float, altura: Float):this(DNI)
+    fun esMayorDeEdad():Boolean{
+        return edad>=18
+    }
 }
 
 
 
 fun main(){
 
-    val yo = Persona("550428F")
+    val yo = Persona("55042809F","Alejandro", 21, 'H')
 }
