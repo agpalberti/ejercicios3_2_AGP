8cd66b7881cfb57589f69b2147ba5fd1fef7be0b
agpalberti <agonzalezp@iesrafaelalberti.es>
2021-11-02 04:41:11 +0100

    TERMINADA CLASE PERSONA

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,7 @@
+import kotlin.random.Random
 
-class Persona(val DNI: String){
-
+class Persona(){
+    val DNI:String
     private var nombre:String = ""
     private var edad:Int = 0
     private var sexo:Char = 'H'
@@ -8,16 +9,16 @@ class Persona(val DNI: String){
             if(value == 'H' || value == 'M') field = value
             else throw IllegalArgumentException("El sexo solo puede ser H o M.")
         }
-    private var peso:Float = 0.0F
-    private var altura: Float = 0.0F
+    private var peso:Float = 1.0F
+    private var altura: Float = 1.0F
 
-    constructor(DNI:String, nombre: String,edad: Int, sexo: Char):this(DNI){
+    constructor(nombre: String,edad: Int, sexo: Char):this(){
         this.nombre = nombre
         this.edad = edad
         this.sexo = sexo
     }
 
-    constructor(DNI: String, nombre:String, edad:Int, sexo:Char, peso:Float, altura: Float):this(DNI){
+    constructor(nombre:String, edad:Int, sexo:Char, peso:Float, altura: Float):this(){
         this.nombre = nombre
         this.edad = edad
         this.sexo = sexo
@@ -25,12 +26,41 @@ class Persona(val DNI: String){
         this.altura = altura
     }
 
-    fun calcularIMC(){
+    init {
+        require(edad>=0){"La edad no puede ser menor a cero."}
+        require(peso>0){"El peso no puede ser menor o igual a cero."}
+        require(altura>0){"La altura no puede ser menor o igual a cero"}
+
+        DNI = generaDNI()
+    }
+
+    fun calcularIMC():Int{
+        return when{
+            peso/(altura*altura) < 20.0 -> -1
+            peso/(altura*altura) in 20.0..25.0 -> 0
+            peso/(altura*altura) > 25 -> 1
+            else -> 2
+        }
 
     }
 
     fun esMayorDeEdad():Boolean{
         return edad>=18
+    }
+
+    fun comprobarSexo(){
+        if (sexo != 'H' && sexo != 'M') sexo = 'H'
+    }
+
+    override fun toString(): String {
+        return "Nombre: \"$nombre\", DNI: $DNI, edad: $edad, sexo: $sexo, peso: $peso, altura: $altura."
+    }
+
+    fun generaDNI():String{
+        var param = ""
+        repeat(8){param += Random.nextInt(0,9)}
+        param+= Random.nextInt(65,90).toChar()
+        return param
     }
 }
 
@@ -38,5 +68,5 @@ class Persona(val DNI: String){
 
 fun main(){
 
-    val yo = Persona("55042809F","Alejandro", 21, 'H')
+    val yo = Persona("Alejandro", 21, 'H')
 }
